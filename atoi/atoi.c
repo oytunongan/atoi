cbdd2515f975ddf05da57113e7b75bb5b9c705da
Oytun <140172465+oytunongan@users.noreply.github.com>
2024-08-29 17:27:17 +0300

    Add files via upload

---
+++ atoi/atoi.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    // TODO
+    int n = strlen(input);
+    if (input[0] == '\0')
+        return 0;
+    int num = input[n - 1] - '0';
+    input[n - 1] = '\0';
+    // printf("%s, %i, %i\n", input, n, num);
+    return (num + 10 * convert(input));
+}
